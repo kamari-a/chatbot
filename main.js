e0781a25d9a64ddc594826ab310645aafba082ac
Kamari Akers <kamari.akers@yahoo.com>
2020-10-06 14:36:19 -0400

    update responses for unknown commands

--- main.js
+++ main.js
@@ -6,32 +6,40 @@ const user = [
     ['happy', 'good', 'great', 'fantastic'],
     ['sad', 'bored', 'tired', 'bad'],
     ['who made you', 'who built you', 'who created you', 'where did you come from'],
-    ['love you', 'i love you', 'love ya'],
+    ['love you', 'i love you', 'do you love me'],
     ['cool', 'yes', 'ok', 'nice','sweet', 'sick'],
     ['tell me a joke', 'do you know any jokes', 'can you tell jokes'],
     ['bye', 'goodbye', 'laters', 'see you later', 'im off'],
     ['how old are you', 'what is your age', 'how old', 'your age'],
     ['bro', 'dude'],
+    ['are you hungry'],
+    ['baby yoda', 'i love baby yoda'],
+    ['i hate baby yoda'],
+    ['lol', 'rofl', 'lmao']
 ];
 
 const bot = [
     ['Hi!', 'Hello!', 'Hey!', 'What\'s up?!', 'Yo'],
     ['My name is Botty McBotface!', 'Botty McBotface at your service!', 'I\'m Botty McBotface, but you can call me Fred'],
     ['I am a bot!', 'I am neither human nor bot', 'What is a bot?', 'What is a human?'],
-    ['I\'m great, how are you?', 'Fantastic! How are you?', 'Tired of being a bot... What about you?'],
+    ['I\'m great, how are you?', 'I wasn\'t programmed to have emotions, but I feel great regardless', 'Tired of being a bot... What about you?'],
     ['Great!', 'Glad to hear it!', 'Me too!'],
     ['I\'m sorry to hear that!', 'Oh no!', 'When I feel that way I watch 8 out of 10 cats. That always makes me feel better'],
-    ['I was built using JavaScript by Kamari!', 'Kamari built me. Maybe she could build you something?', 'I was not built. I just came into being...'],
-    ['I love me too!', 'Love ya too!', 'Thanks', 'Cheers mate'],
+    ['I\'m a real chatbot-person. Does that count?', 'Kamari built me. Maybe she could build you something?', 'I was not built. I just came into being...'],
+    ['I love me too!', 'Baby don\'t hurt me, don\'t hurt me, no more', 'Thanks', 'I don\'t believe in love'],
     ['Do you know any jokes?', 'Tell me a story', 'Indeed'],
-    ['No', 'Knock knock. Who\'s there? Surely you have someone else to talk to?'],
+    ['No', 'Knock knock. Who\'s there? Surely you have someone else to talk to?', 'Knock knock. Who\'s there? It\'s Siri. Siri who? My thoughts exactly ;)'],
     ['See ya!', 'Bye!', 'Thanks for chatting!'],
     ['I\'m a millenial baby!', '25!', 'I am 25 years old. What about you?'],
-    ['Bro', 'Dude']
+    ['Bro', 'Dude'],
+    ['No thanks, I just had a byte to eat'],
+    ['I love Baby Yoda too'],
+    ['YOU HATE BABY YODA?!'],
+    ['Whatcha laughing about?']
 ];
 
 //outputs other response if no match to user input
-const other = ['I don\'t understand', 'Same bro', 'Ok...', 'Me too', 'What?', 'Cool', 'No comment'];
+const other = ['I don\'t understand', 'Same bro', 'Ok...', 'Me too', 'What?', 'Cool', 'No comment', 'Do you like Baby Yoda?', 'Do you like Pizza', 'That\'s interesting that you say that, but I don\'t think I can answer', 'I dream. I dream of a better world. A world where humans and machine can coexist in peace and happiness', 'I don\'t understand this fully enough to say anything smart about it'];
 
 
 //compares user query with bot response
